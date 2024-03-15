df7a127f2b2a9a1ebd44a3487af5c18fa19e48e8
Sandeep Kumar <sandeepkumar8713@gmail.com>
2024-03-15 18:40:47 +0530

    Mockito working

--- src/test/java/com/example/springBootDemo/ValidatorTest.java
+++ src/test/java/com/example/springBootDemo/ValidatorTest.java
@@ -1,20 +1,35 @@
 package com.example.springBootDemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.mock;
 
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class ValidatorTest {
     private static final Logger logger = LoggerFactory.getLogger(ValidatorTest.class);
 
+    @MockBean
+    Validator validator;
+
     @Test
     public void testMock(){
-        Validator obj = new Validator();
-        String result = obj.checking("jh");
+
+        Mockito.when(validator.checking("jh")).thenReturn("Mocking method");
+
+        String result = validator.checking("jh");
 
         logger.info("Result : " + result);
-        assert result.equals("Main method");
+        assert result.equals("Mocking method");
 
     }
 }
