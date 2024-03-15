fb1749c9c975aa73dfc8d865685f258f4cbf2dfc
Sandeep Kumar <sandeepkumar8713@gmail.com>
2024-03-15 18:48:53 +0530

    small fix

--- src/test/java/com/example/springBootDemo/ValidatorTest.java
+++ src/test/java/com/example/springBootDemo/ValidatorTest.java
@@ -1,11 +1,9 @@
 package com.example.springBootDemo;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
