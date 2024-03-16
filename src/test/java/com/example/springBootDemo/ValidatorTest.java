8fbc9785052dd9fb2827f099cfb33f40488e36a3
Sandeep Kumar <sandeepkumar8713@gmail.com>
2024-03-16 12:28:02 +0530

    comments added

--- src/test/java/com/example/springBootDemo/ValidatorTest.java
+++ src/test/java/com/example/springBootDemo/ValidatorTest.java
@@ -8,13 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.ArgumentMatchers.any;
 
 
-@SpringBootTest
+// @SpringBootTest // This will load the entire application
+// Make sure that entire application context is loaded only once during test case.
+@SpringBootTest(classes = Validator.class) // This will load only the required classes and will be faster.
 @ExtendWith(SpringExtension.class)
+//@ActiveProfiles("sandbox") // This will load spring profile for testing i.e. different application.properties
 public class ValidatorTest {
     private static final Logger logger = LoggerFactory.getLogger(ValidatorTest.class);
 
